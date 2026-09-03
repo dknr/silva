cb844ea25c0965c1cf7ded5d62141bec3d8993b2
David Knauer <dknr@caos.one>
2026-09-03 05:50:49 -0700

    feat: add Telegram bot integration using gram.io

--- silva.ts
+++ silva.ts
@@ -1,6 +1,8 @@
+import {Bot} from 'gramio';
 import z from "zod";
 
 import { createAgent, ToolRegistry, tool } from "./agent/mod.ts";
+import { createAsyncQueue } from "./agent/queue.ts";
 
 const tools: ToolRegistry = {
     time: tool(z.object(), () => Promise.resolve(new Date().toISOString())),
@@ -31,19 +33,37 @@ const tools: ToolRegistry = {
     ),
 }
 
+const allowedId = parseInt(Deno.env.get('TG_ALLOWED_ID') ?? '', 10);
+if (!allowedId) throw new Error();
+
+const bot = new Bot(Deno.env.get('TG_TOKEN') ?? '');
+bot.on('message', (context) => {
+    if (context.from.id !== allowedId) return;
+    if (!context.hasText()) return;
+    console.log(context.text);
+    agent.send(context.text);
+});
+
+bot.start();
+bot.api.sendMessage({chat_id: allowedId, text: 'bot started'});
+
 const agent = createAgent({
     baseUrl: 'http://10.11.116.184:8002',
     model: '',
     tools,
 }, (e) => {
     try {
-        if (e.tool_calls?.length) {
-            console.log('🔨', e.tool_calls[0].function.name, e.tool_calls[0].function.arguments);
-        } else if (e.role === 'tool') {
-            // no-op
-        } else {
-            console.log(e.role, e.content)
+        if (e.role === 'assistant' && e.content) {
+            bot.api.sendMessage({chat_id: allowedId, text: e.content});
         }
+        console.log(e);
+        // if (e.tool_calls?.length) {
+        //     console.log('🔨', e.tool_calls[0].function.name, e.tool_calls[0].function.arguments);
+        // } else if (e.role === 'tool') {
+        //     // no-op
+        // } else {
+        //     console.log(e.role, e.content)
+        // }
     } catch {
         console.log(e);
     }
